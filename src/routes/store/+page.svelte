cc9ded69782b68cb7065fe2321953f70c0df8b27
Denis <sustain@bk.ru>
2024-07-20 17:35:42 +0300

    chapter 7 (all broken)

---
+++ src/routes/store/+page.svelte
@@ -0,0 +1,6 @@
+<h3>Welcome to the store!</h3>
+
+<ul>
+  <li><a href='/locations'>Locations</a></li>
+  <li><a href='/products'>Products</a></li>
+</ul>
---
+++ src/routes/store/locations/+layout.svelte
@@ -0,0 +1,8 @@
+<ul>
+  <li><a href='/store/locations'>North America</a></li>
+  <li><a href='/store/locations/ca'>Canada</a></li>
+  <li><a href='/store/locations/me'>Mexico</a></li>
+  <li><a href='/store/locations/us'>United States</a></li>
+</ul>
+
+<slot />
